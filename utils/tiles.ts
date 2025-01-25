7e4ea05a58be4fb7876cb8f529420f3620c1094d
bryanng <bryanng412@gmail.com>
2025-01-24 23:52:18 -0800

    refactor utils

--- utils/tiles.ts
+++ utils/tiles.ts
@@ -2,15 +2,22 @@ import { Wind } from '@/store/windSlice'
 
 export const MAX_HAND_SIZE = 14
 export const MAX_NUMBER_OF_SINGLE_TILE = 4
+export const MAX_NUMBER_OF_5_TILE = 3
+
+export const convertStringTileToNumber = (tile: string): number =>
+  Number(tile.replace(/[^0-9]/g, ''))
 
 //Man, Pin, and Sou tiles are 0-27
-export const is5Tile = (tile: number): boolean => tile % 9 === 4 && tile < 27
+export const is5Tile = (tile: string): boolean => {
+  const numTile = convertStringTileToNumber(tile)
+  return numTile % 9 === 4 && numTile < 27
+}
 
 export const generateAllTiles = (): string[] => {
   const tiles = []
   for (let i = 0; i < 34; i++) {
     tiles.push(i.toString())
-    if (is5Tile(i)) {
+    if (is5Tile(i.toString())) {
       tiles.push(`${i}-dora`)
     }
   }
@@ -20,8 +27,12 @@ export const generateAllTiles = (): string[] => {
 
 export const sortTiles = (tiles: string[]) =>
   [...tiles].sort((a, b) => {
-    const numA = parseFloat(a)
-    const numB = parseFloat(b)
+    const numA = convertStringTileToNumber(a)
+    const numB = convertStringTileToNumber(b)
+
+    if (numA === numB) {
+      return a.localeCompare(b)
+    }
     return numA - numB
   })
 
@@ -30,11 +41,14 @@ export const canAddTile = (tiles: string[], t: string): boolean => {
     (count, tile) => (tile === t ? count + 1 : count),
     0
   )
-  const isDoraAlreadyTaken = t.includes('dora')
+  const isDoraAlreadyUsed = t.includes('dora')
     ? tiles.some(tile => tile === t)
     : false
+  const maxNumberOfTiles = is5Tile(t)
+    ? MAX_NUMBER_OF_5_TILE
+    : MAX_NUMBER_OF_SINGLE_TILE
 
-  return tilesUsed < MAX_NUMBER_OF_SINGLE_TILE && !isDoraAlreadyTaken
+  return tilesUsed < maxNumberOfTiles && !isDoraAlreadyUsed
 }
 
 export const isWindTile = (tile: string): boolean =>
