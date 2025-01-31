35c7d9b6cf4d767062240321cad94e1e97afeabb
bryanng <bryanng412@gmail.com>
2025-01-31 15:55:48 -0800

    add fu

--- components/Calculations.tsx
+++ components/Calculations.tsx
@@ -14,75 +14,81 @@ const Calculations = ({ calcData }: CalculationsProps) => {
   const results = calculateHand(calcData)
 
   return results.map(
-    ({ result: { error, yaku, han, ten, yakuman, fu }, hand }, i) =>
-      !error &&
-      ten > 0 &&
-      (yakuman === 0 ? (
-        <Fragment key={i}>
-          <Table.Root width={['100%', null, '70%']} size="lg" marginY="3">
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeader fontWeight="semibold">
-                  Yaku
-                </Table.ColumnHeader>
-                <Table.ColumnHeader fontWeight="semibold" textAlign="end">
-                  Han
-                </Table.ColumnHeader>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {Object.keys(yaku).map((y, i) => (
-                <Table.Row key={`${y}-${i}`}>
-                  <Table.Cell>
-                    {YakuInfo[y as keyof typeof YakuInfo].en}
-                    <InfoTip
-                      content={YakuInfo[y as keyof typeof YakuInfo].tooltipEN}
-                    />
-                  </Table.Cell>
-                  <Table.Cell textAlign="end">{yaku[y]}</Table.Cell>
+    ({ result: { error, yaku, han, ten, yakuman, fu }, hand }, i) => {
+      const hanName = getHanName(han, fu)
+
+      return (
+        !error &&
+        ten > 0 &&
+        (yakuman === 0 ? (
+          <Fragment key={i}>
+            <Table.Root width={['100%', null, '70%']} size="lg" marginY="3">
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeader fontWeight="semibold">
+                    Yaku
+                  </Table.ColumnHeader>
+                  <Table.ColumnHeader fontWeight="semibold" textAlign="end">
+                    Han
+                  </Table.ColumnHeader>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Root>
-          <CalculationTileDisplay hand={hand} />
-          <Text textStyle="xl" fontWeight="medium" textAlign="center">
-            {han} Han - {getHanName(han, fu)}
-          </Text>
-          <Text textStyle="xl" fontWeight="medium" textAlign="center">
-            {ten}
-          </Text>
-        </Fragment>
-      ) : (
-        <Fragment key={i}>
-          <Text textStyle="xl" fontWeight="medium" textAlign="center">
-            Yakuman
-          </Text>
-          <Separator size="md" width={['100%', null, '60%']} />
-          {Object.keys(yaku).map((y, i) => (
-            <Text
-              key={`${y}-${i}`}
-              textStyle="xl"
-              fontWeight="medium"
-              textAlign="center"
-            >
-              {YakuInfo[y as keyof typeof YakuInfo].en}
-              <InfoTip
-                content={YakuInfo[y as keyof typeof YakuInfo].tooltipEN}
-              />
-            </Text>
-          ))}
-          <Separator size="md" width={['100%', null, '60%']} />
-          <CalculationTileDisplay hand={hand} />
-          {yakuman > 1 && (
+              </Table.Header>
+              <Table.Body>
+                {Object.keys(yaku).map((y, i) => (
+                  <Table.Row key={`${y}-${i}`}>
+                    <Table.Cell>
+                      {YakuInfo[y as keyof typeof YakuInfo].en}
+                      <InfoTip
+                        content={YakuInfo[y as keyof typeof YakuInfo].tooltipEN}
+                      />
+                    </Table.Cell>
+                    <Table.Cell textAlign="end">{yaku[y]}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+            <CalculationTileDisplay hand={hand} />
             <Text textStyle="xl" fontWeight="medium" textAlign="center">
-              {numberToPrefix(yakuman)} Yakuman
+              {han} Han{fu > 0 && ` ${fu} Fu`}
+              {hanName && ` - ${hanName}`}
             </Text>
-          )}
-          <Text textStyle="xl" fontWeight="medium" textAlign="center">
-            {ten}
-          </Text>
-        </Fragment>
-      ))
+            <Text textStyle="xl" fontWeight="medium" textAlign="center">
+              {ten}
+            </Text>
+          </Fragment>
+        ) : (
+          <Fragment key={i}>
+            <Text textStyle="xl" fontWeight="medium" textAlign="center">
+              Yakuman
+            </Text>
+            <Separator size="md" width={['100%', null, '60%']} />
+            {Object.keys(yaku).map((y, i) => (
+              <Text
+                key={`${y}-${i}`}
+                textStyle="xl"
+                fontWeight="medium"
+                textAlign="center"
+              >
+                {YakuInfo[y as keyof typeof YakuInfo].en}
+                <InfoTip
+                  content={YakuInfo[y as keyof typeof YakuInfo].tooltipEN}
+                />
+              </Text>
+            ))}
+            <Separator size="md" width={['100%', null, '60%']} />
+            <CalculationTileDisplay hand={hand} />
+            {yakuman > 1 && (
+              <Text textStyle="xl" fontWeight="medium" textAlign="center">
+                {numberToPrefix(yakuman)} Yakuman
+              </Text>
+            )}
+            <Text textStyle="xl" fontWeight="medium" textAlign="center">
+              {ten}
+            </Text>
+          </Fragment>
+        ))
+      )
+    }
   )
 }
 
