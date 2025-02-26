0c47842b3c1d32c24e72ca97fc645438ff9f6ac7
bryanng <bryanng412@gmail.com>
2025-02-25 16:01:39 -0800

    add sw

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from 'next'
+import withSerwistInit from '@serwist/next'
 
-const nextConfig: NextConfig = {
+const withSerwist = withSerwistInit({
+  swSrc: 'app/sw.ts', // where the service worker src is
+  swDest: 'public/sw.js', // where the service worker code will end up
+})
+
+export default withSerwist({
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   },
-}
-
-export default nextConfig
+})
